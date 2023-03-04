00cc47c8c0bdfbda66407f0126e8f4d36d8dd05a
Raulscj <radscjggdiosteama@gmail.com>
2023-03-04 17:10:16 -0400

    toggle btn

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,28 +2,33 @@ import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
 
+const boton = document.querySelector('#btn_nav')
+const menu = document.querySelector('#menu')
+boton.addEventListener('click',()=>{
+  menu.classList.toggle('hidden')
+})
 export default function Navbar() {
   return (
     <>
-      <nav class="w-28">
-        <button class="m-2 bg-gray-700 hover:bg-gray-900 text-white py-2 px-3 rounded-md">
+      <nav className="w-28">
+        <button id='btn_nav' className="m-2 bg-gray-700 hover:bg-gray-900 text-white py-2 px-3 rounded-md lg:hidden">
           <FontAwesomeIcon icon={faBars} />
         </button>
-        <ul className="bg-green-400 px-2 py-4">
-          <li className="uppercase text-xs" href="">
-            Inicio
+        <ul id="menu" className="bg-green-400 px-2 py-4">
+          <li>
+            <a className="uppercase text-xs" href=""> Inicio</a> 
           </li>
-          <li className="uppercase text-xs" href="">
-            ¿Quienes somos?
+          <li>
+            <a className="uppercase text-xs" href="">¿Quienes somos?</a> 
           </li>
-          <li className="uppercase text-xs" href="">
-            Nuestros Clientes
+          <li>
+            <a className="uppercase text-xs" href="">Nuestros Clientes</a> 
           </li>
-          <li className="uppercase text-xs" href="">
-            Productos
+          <li>
+            <a className="uppercase text-xs" href="">Productos </a> 
           </li>
-          <li className="uppercase text-xs" href="">
-            Como comprar
+          <li>
+            <a className="uppercase text-xs" href="">Como comprar</a> 
           </li>
         </ul>
       </nav>
