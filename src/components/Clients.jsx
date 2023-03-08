cf873e329338b174a10a830aa45a2d6a7203634d
Raulscj <92169825+Raulscj@users.noreply.github.com>
2023-03-08 12:50:14 -0400

    contenido

--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -1,5 +1,34 @@
-import React from "react";
+import React /* , { useState, useEffect } */ from "react";
+const clientes = [
+  {
+    id: 1,
+    path: "",
+    titulo: "Good",
+  },
+  {
+    id: 2,
+    path: "",
+    titulo: "Bad",
+  },
+];
 
 export default function Clients() {
-  return <div>Clients</div>;
+  return (
+    <>
+      {clientes.map((cliente) => (
+        <div key={cliente.id} className="card small">
+          <div className="card">
+            <img
+              className="mainPic"
+              src={`${cliente.path}`}
+              alt={cliente.titulo}
+            />
+            <div className="contenidoCard">
+              <h3>{cliente.titulo}</h3>
+            </div>
+          </div>
+        </div>
+      ))}
+    </>
+  );
 }
